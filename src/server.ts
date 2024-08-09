df60c8631a18ff088947bea899576abc91a70f67
JoaoErick <jerick1415@outlook.com>
2024-08-09 12:21:29 -0300

    feat: add api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import cors from '@fastify/cors';
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
 import { confirmParticipant } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
 import { createTrip } from "./routes/create-trip";
@@ -20,6 +22,23 @@ const app = fastify()
 
 app.register(cors, {
   origin: '*',
+})
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'plann.er',
+      description: 'Especificações da API para o back-end da aplicação plann.er construída durante o NLW Journey da Rocketseat',
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
 })
 
 app.setValidatorCompiler(validatorCompiler)
