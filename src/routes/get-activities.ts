3db53c812b925606a19a37e67a37e9bf22d602ad
JoaoErick <jerick1415@outlook.com>
2024-09-20 08:11:37 -0300

    chore: remove imports

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -1,9 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import nodemailer from 'nodemailer';
 import { z } from "zod";
 import { dayjs } from "../lib/dayjs";
-import { getMailClient } from "../lib/mail";
 import { prisma } from "../lib/prisma";
 import { ClientError } from "../errors/client-error";
 
